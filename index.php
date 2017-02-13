93c36ecf1b2232560870cf8b79a8bff17f7d9889
Russell Pena <russellpena@Russells-MacBook-Pro.local>
2017-02-12 23:44:56 -0500

    VERY rough draft of my bootstrap portfolio

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Project: FullStack-Lesson-Plans
+ * @license GPL v2
+ * Build: @BUILD_DATE@
+ * Version: @VERSION@
+ */
+include ('index.html');
+include ('portfolio.html')
+include ('contact.html')
